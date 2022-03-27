2594d6e3682938fac6b90e7be865b1e08950d9d2
Niki4_37 <redcapet@mail.ru>
2022-03-27 17:34:39 +0300

    Step 02. Base Weapon class. Add character's animation

---
+++ Source/EducationalProject/Private/Weapon/EPBaseWeapon.cpp
@@ -0,0 +1,20 @@
+// For educational purposes only.
+
+
+#include "Weapon/EPBaseWeapon.h"
+
+AEPBaseWeapon::AEPBaseWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+    
+    WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>("WeaponMesh");
+    SetRootComponent(WeaponMesh);
+
+}
+
+void AEPBaseWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/EducationalProject/Public/Weapon/EPBaseWeapon.h
@@ -0,0 +1,31 @@
+// For educational purposes only.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Components/SkeletalMeshComponent.h"
+
+#include "EPBaseWeapon.generated.h"
+
+class USkeletalMeshComponent;
+
+UCLASS()
+class EDUCATIONALPROJECT_API AEPBaseWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AEPBaseWeapon();
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+    USkeletalMeshComponent* WeaponMesh;
+
+	virtual void BeginPlay() override;
+
+public:	
+
+
+};
